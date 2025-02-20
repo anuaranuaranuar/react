f74e2793caa1d0b026121d0bc8292ad993bc8b2a
Anuar Parejo <anuarparejo@hotmail.com>
2025-02-20 01:43:58 -0500

    primer commit

---
+++ Mi-primer_react/src/App.jsx
@@ -0,0 +1,24 @@
+export function App() { //creo una funcion componente para mostrar en main.jsx
+  return (
+    //los estilos en linea se ponen como un objecto <article style={{display : "flex"}}>
+    //se utiliza className en lugar de class para declarar una clase ya que class es reservada 
+    <article className="tw-followCard" style={{display : "flex", color: "#fff", alignItems:"center", }}>
+      <header className="tw-followCard-header">
+        <img 
+        className="tw-followCard-avatar"
+        src="https://unavatar.io/midudev" 
+        alt="avatar dudev" />
+        <div className="tw-followCard-info">
+          <strong>Miguel angel</strong>
+          <span className="tw-followCard-infoUserName">@midudev</span>
+        </div>
+      </header>
+
+      <aside>
+        <button  className="tw-followCard-button">
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
+}
---
+++ Mi-primer_react/src/main.jsx
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {App} from './App.jsx'
+
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+ 
+  <React.Fragment>
+  <App />
+  </React.Fragment>
+)
